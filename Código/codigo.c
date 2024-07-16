9528760863e265c584bbb500d66f528f86fc5b0a
Vitor Taira <vitor.taira@catijr.com.br>
2024-07-15 22:59:30 -0300

    commentary add

--- Código/codigo.c
+++ Código/codigo.c
@@ -29,24 +29,27 @@ sem_t serf_queue;
 
 void *routine(void *args)
 {
+    // sleep para simular a chegada de pessoas (hackers e serfs)
     sleep(rand() % 5); // sleep para simular a chegada de pessoas (hackers e serfs)
+    // Define se é hacker (0) ou serf (1)
     int index = *(int *)args;
     bool isCaptain = false;
 
+    // Mutex para proteger a seção crítica onde hackers e serfs são contados
     pthread_mutex_lock(&mutex);
-    if (index == 0) // caso hackers entrarem no barco
+    if (index == 0) // Caso hackers entrarem no barco
     {
         hackers++;
-        if (hackers == 4)
+        if (hackers == 4) // Se há 4 hackers, eles formam um grupo e entram no barco
         {
             sem_post(&hacker_queue);
             sem_post(&hacker_queue);
             sem_post(&hacker_queue);
             sem_post(&hacker_queue);
             hackers -= 4;
-            isCaptain = true;
+            isCaptain = true; // um hacker é designado como hacker
         }
-        else if (hackers == 2 && serfs >= 2)
+        else if (hackers == 2 && serfs >= 2) // Se há 2 hackers ou 2 serfs, eles formam um grupo e podem entrar no barco
         {
             sem_post(&hacker_queue);
             sem_post(&hacker_queue);
@@ -54,26 +57,26 @@ void *routine(void *args)
             sem_post(&serf_queue);
             hackers -= 2;
             serfs -= 2;
-            isCaptain = true;
+            isCaptain = true; // Um hacker ou serf é designado como capitão
         }
         else
         {
-            pthread_mutex_unlock(&mutex);
+            pthread_mutex_unlock(&mutex); // Libera o mutex se não formou o grupo
         }
     }
     else
     { // caso serfs entrarem no barco
         serfs++;
-        if (serfs == 4)
+        if (serfs == 4) // Se há 4 serfs, formam um grupo e entram no barco
         {
             sem_post(&serf_queue);
             sem_post(&serf_queue);
             sem_post(&serf_queue);
             sem_post(&serf_queue);
             serfs -= 4;
-            isCaptain = true;
+            isCaptain = true; // Um serf é capitão
         }
-        else if (hackers >= 2 && serfs >= 2)
+        else if (hackers >= 2 && serfs >= 2) // // Se há 2 hackers ou 2 serfs, eles formam um grupo e podem entrar no barco
         {
             sem_post(&hacker_queue);
             sem_post(&hacker_queue);
@@ -81,14 +84,15 @@ void *routine(void *args)
             sem_post(&serf_queue);
             hackers -= 2;
             serfs -= 2;
-            isCaptain = true;
+            isCaptain = true; // Ou um hacker ou um serf é capitão
         }
         else
         {
-            pthread_mutex_unlock(&mutex);
+            pthread_mutex_unlock(&mutex); // Libera o mutex se não formou o grupo
         }
     }
 
+    // Hacker ou serf espera até que seu semáforo seja liberado
     if (index == 0) // faz o semáforo esperar o valor de um semáforo (hacker)
     {
         sem_wait(&hacker_queue);
@@ -104,6 +108,7 @@ void *routine(void *args)
         pthread_mutex_unlock(&boat_mutex);
     }
 
+    // Função board quando entra no barco
     board(index);
 
     // Implementação da barreira usando variável de condição e mutex
@@ -112,7 +117,7 @@ void *routine(void *args)
     if (barrier_count == boatNumber)
     {
         barrier_count = 0;
-        pthread_cond_broadcast(&barrier_cond);
+        pthread_cond_broadcast(&barrier_cond); // Desbloqueia todos quando 4 threads estão na barreira
     }
     else
     {
@@ -126,8 +131,8 @@ void *routine(void *args)
         printf("Serfs no barco: %d\n", serfs_on_boat);
         hackers_on_boat = 0;
         serfs_on_boat = 0;
-        rowboard(index);
-        pthread_mutex_unlock(&mutex);
+        rowboard(index);              // Capitão chama a função para remar
+        pthread_mutex_unlock(&mutex); // Capitão libera o mutex para permitir novos grupos
     }
 
     free(args);
@@ -136,6 +141,7 @@ void *routine(void *args)
 
 int main()
 {
+    // Variáveis
     pthread_t th[ThreadsMAX];
     pthread_mutex_init(&mutex, NULL);
     pthread_mutex_init(&boat_mutex, NULL);
@@ -146,17 +152,20 @@ int main()
     int pessoas[ThreadsMAX];
     int num_hackers, num_serfs;
 
+    // Escolha de quantidade de hacker e serfs
     printf("Digite a quantidade de hackers: ");
     scanf("%d", &num_hackers);
     printf("Digite a quantidade de serfs: ");
     scanf("%d", &num_serfs);
 
+    // Caso estoure o limite máximo
     if (num_hackers + num_serfs > ThreadsMAX)
     {
         printf("Erro: A soma de hackers e serfs excede o limite máximo de %d.\n", ThreadsMAX);
         return 1;
     }
 
+    // Inicia o array com hackers ou serfs
     for (int i = 0; i < num_hackers; i++, cont_pessoa++)
     {
         pessoas[cont_pessoa] = 0; // 0 representa hacker
@@ -167,6 +176,7 @@ int main()
         pessoas[cont_pessoa] = 1; // 1 representa serf
     }
 
+    // Criação de threads
     for (int i = 0; i < cont_pessoa; i++)
     {
         int *a = malloc(sizeof(int));
@@ -177,6 +187,7 @@ int main()
         }
     }
 
+    // Espera todas as threads terminarem
     for (int i = 0; i < cont_pessoa; i++)
     {
         if (pthread_join(th[i], NULL) != 0)
@@ -185,6 +196,7 @@ int main()
         }
     }
 
+    // Destrói mutex, variáveis de condição e semáforos
     pthread_mutex_destroy(&mutex);
     pthread_mutex_destroy(&boat_mutex);
     pthread_cond_destroy(&barrier_cond);
